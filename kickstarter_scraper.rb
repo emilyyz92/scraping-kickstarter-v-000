8312de2398472312a9d8733a89d80d215541fff2
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-05 21:41:21 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
 
   #iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    project[project] = {}
+    projects[project] = {}
   end
 
   projects
