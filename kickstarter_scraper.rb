93a679d896b088ba2528cafcdd116adaba6754a8
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-05 21:54:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,18 +10,20 @@ def create_project_hash
   #iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {} #converting title to symbol
+    projects[title.to_sym] = {
+      projects: kickstarter.css("li.project.grid_4")
+      title: project.css("h2.bbcard_name strong a").text
+      image link: project.css("div.project-thumbnail a img").attribute("src").value
+      description: project.css("p.bbcard_blurb").text
+      location: project.css("ul.project-meta span.location-name").text
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+    } #converting title to symbol
   end
 
   projects
 end
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
 create_project_hash
